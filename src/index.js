406491b6647ec113b81fc3ff9fdeb8eb95409128
Hamza <144457566+unk-user@users.noreply.github.com>
2023-11-17 23:02:45 +0100

    UI improvements and new features

--- src/index.js
+++ src/index.js
@@ -35,11 +35,18 @@ class Task {
 }
 
 class Project {
-    constructor(title) {
+    constructor(title, mode) {
         this.title = title;
+        this.mode = mode;
         this.tasks = [];
     }
 
+    getMode() {
+        return this.mode;
+    }
+    setMode(mode) {
+        this.mode = mode;
+    }
     getTitle() {
         return this.title;
     }
@@ -61,19 +68,19 @@ class Project {
     containsTask(title) {
         return this.tasks.some((task) => task.getTitle() === title);
     }
-    removeTask(title) {
-        this.tasks = this.tasks.filter((task) => task.getTitle() !== title);
+    removeTask(task) {
+        this.tasks = this.tasks.filter((Item) => Item !== task);
     }
 }
 
 //TodoList
 
-class TodoList {
+class Todo {
     constructor(){
         this.projects = [];
-        this.projects.push(new Project('Inbox'));
-        this.projects.push(new Project('Today'));
-        this.projects.push(new Project('school'));
+        this.projects.push(new Project('Inbox', 'default'));
+        this.projects.push(new Project('Today', 'default'));
+        this.projects.push(new Project('school', 'user'));
     }
     getProjectByTitle(title){
         return this.projects.find((project) => project.getTitle() === title);
@@ -87,8 +94,8 @@ class TodoList {
     setProjects(projects){
         this.projects = projects;
     }
-    removeProject(title){
-        this.projects = this.projects.filter((project) => project.getTitle() !== title);
+    removeProject(project){
+        this.projects = this.projects.filter((items) => items !== project);
     }
     containsProject(title){
         return this.project.some((project) => project.getTitle() === title);
@@ -103,7 +110,7 @@ class Storage {
     }
     static getTodoList(){ 
         const todoList = Object.assign(
-            new TodoList(),
+            new Todo(),
             JSON.parse(localStorage.getItem('todolist'))
         )
 
@@ -144,6 +151,183 @@ navCheck.addEventListener('click', ()=>{
 
 
 //Main UI
+
+
+//Main UI 
+
+    let todo = (createTodoList = () => {
+        let todo;
+        if(typeof(localStorage.getItem('todolist')) == 'undefined'){
+            todo = new Todo();
+        } else {
+            todo = Storage.getTodoList();
+        }
+        return todo;
+    })();
+
+class UI {
+    
+    static refreshNav() {
+        const previewUser = document.querySelector('#userProjects');
+        let lastButtons = document.querySelectorAll('.user');
+        lastButtons.forEach((button) => button.remove());
+        
+        
+        todo.getProjects().forEach((project) => {
+            let projectPreview = document.createElement('button');
+            
+            projectPreview.setAttribute('id', `${project.getTitle()}Btn`);
+            projectPreview.classList += 'navBtn link user';
+            projectPreview.textContent = `${project.getTitle()}`;
+    
+            if(project.getMode() === 'user'){
+                let removeBtn = document.createElement('div');
+                removeBtn.className = 'removeProject';
+                removeBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>';
+                projectPreview.appendChild(removeBtn);
+                this.projectEventListeners(projectPreview, project, removeBtn);
+                previewUser.appendChild(projectPreview);
+            }
+        })
+    }
+    static loadNavItems() {
+        const previewDefault = document.querySelector('#defaultProjects');
+        const previewUser = document.querySelector('#userProjects');
+
+        todo.getProjects().forEach((project) => {
+            let projectPreview = document.createElement('button');
+            
+            projectPreview.setAttribute('id', `${project.getTitle()}Btn`);
+            projectPreview.classList += 'navBtn link';
+            projectPreview.textContent = `${project.getTitle()}`;
+    
+            if(project.getMode() === 'default'){
+                previewDefault.appendChild(projectPreview);
+                this.projectEventListeners(projectPreview, project, 'none');
+            } else {
+                let removeBtn = document.createElement('div');
+                removeBtn.className = 'removeProject';
+                projectPreview.classList += ' user';
+                removeBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>';
+                projectPreview.appendChild(removeBtn);
+                this.projectEventListeners(projectPreview, project, removeBtn);
+                previewUser.appendChild(projectPreview);
+            }
+        })
+    }
+
+    static projectEventListeners(projectPreview, project, removeBtn) {
+
+        projectPreview.addEventListener('mousedown', () => {
+            document.querySelector('#tasksContainer').innerHTML = '<div id="addTask">+ Add task</div>';
+            this.setActiveNav(projectPreview);
+            this.loadTasks(project);
+        })
+        if(removeBtn !== 'none'){
+            removeBtn.addEventListener('mouseup', () => {
+                this.setActiveNav(projectPreview);
+                this.loadTasks(todo.getProjectByTitle('Inbox'));
+                this.deleteProject(project);
+            });
+        }
+    }
+    static setActiveNav(projectP) {
+        document.querySelectorAll('.link').forEach((btn) => {
+            if(btn.classList === 'navBtn link activeNav'){
+                btn.classList = 'navBtn link';
+            } else {
+                btn.classList = 'navBtn link user';
+            }
+        })
+        projectP.classList += ' activeNav';
+    }
+
+    static loadTasks(project) {     
+        project.getTasks().forEach((task) => {
+            let taskDiv = document.createElement('div');
+            taskDiv.setAttribute('id', `${task.getTitle()}`);
+            taskDiv.classList += 'task';
+            taskDiv.innerHTML = `
+                <div class="taskLeft">
+                    <p>${task.getTitle()}</p>
+                    <p id="description">${task.getDesc()}</p>
+                </div>
+                <div class="taskRight">
+                    <p>${task.getDate()}</p>
+                    <button id="modifyBtn" class="taskBtn">M</button>
+                    <button id="removeBtn" class="taskBtn">X</button>
+                </div>`;
+            document.querySelector('#tasksContainer').appendChild(taskDiv);
+        });
+    }
+
+    static deleteProject(project) {
+        todo.removeProject(project);
+        document.querySelector(`#${project.getTitle()}Btn`).remove();
+        Storage.saveTodoList(todo);
+    }
+
+    static loadUi() {
+        this.loadNavItems();
+        this.loadTasks(todo.getProjectByTitle('Inbox'));
+        this.openNewProjectL();
+
+
+    }
+
+    static openNewProjectL() {
+        const addBtn = document.querySelector('#addProject');
+        const previewUser = document.querySelector('#userProjects');
+
+        addBtn.addEventListener('click', () => {
+            let newProjectCard = document.createElement('div');
+            newProjectCard.setAttribute('id', 'newProjectCard');
+            
+            const input = document.createElement('input');
+            input.setAttribute('id', 'newProjectTitle');
+            input.setAttribute('type', 'name');
+            newProjectCard.appendChild(input);
+            const newProjectAction = document.createElement('div');
+            newProjectAction.setAttribute('id', 'newProjectAction');
+            const saveNewProject = document.createElement('button');
+            const cancelNewProject = document.createElement('button');
+            saveNewProject.setAttribute('id', 'saveNewProject');
+            saveNewProject.textContent = 'Save';
+            cancelNewProject.setAttribute('id', 'cancelNewProject');
+            cancelNewProject.textContent = 'Cancel';
+ 
+            newProjectAction.appendChild(saveNewProject);
+            newProjectAction.appendChild(cancelNewProject);
+            newProjectCard.appendChild(newProjectAction);
+
+            saveNewProject.addEventListener('click', () => {
+                if(input.value !== ''){
+                    let newProject = new Project(input.value, 'user');
+                    todo.addProject(newProject);
+                    Storage.saveTodoList(todo);
+                    this.closeNewProjectL();
+                }
+            });
+            cancelNewProject.addEventListener('click', () => {
+                this.closeNewProjectL()
+            })
+
+            previewUser.appendChild(newProjectCard);
+            
+        });
+    }
+
+    static closeNewProjectL() {
+        document.querySelector('#newProjectTitle').value = '';
+        document.querySelector('#newProjectCard').remove();
+        this.refreshNav();
+    }
+
+}
+
+UI.loadUi();
+todo.getProjectByTitle('school').addTask(new Task('hew'));
+
 
 
 
