257da0976a00df398e08f57740101611d4df5692
Hamza <144457566+unk-user@users.noreply.github.com>
2023-11-20 23:07:16 +0100

    minor style improvements

--- src/index.js
+++ src/index.js
@@ -264,7 +264,7 @@ class UI {
         Storage.saveTodoList(todo);
     }
     static loadTasks(project) {
-        if(project !== todo.getProjectByTitle('Today')){document.querySelector('#tasksContainer').innerHTML = '<div id="addTask">+ Add task</div>'}
+        if(project !== todo.getProjectByTitle('Today')){document.querySelector('#tasksContainer').innerHTML = '<div id="addTask"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg> Add task</div>'}
         else {document.querySelector('#tasksContainer').innerHTML = ''}
         project.getTasks().forEach((task) => {
             let taskDiv = document.createElement('div');
@@ -278,8 +278,8 @@ class UI {
                 <div class="taskRight">
                     <p>${task.getDate()}</p>
                     <p>${task.getPrio()}</p>
-                    <button id="modifyBtn" class="taskBtn">M</button>
-                    <button id="removeBtn" class="taskBtn">X</button>
+                    <button id="modifyBtn" class="taskBtn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg></button>
+                    <button id="removeBtn" class="taskBtn"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="21" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></button>
                 </div>`;
             document.querySelector('#tasksContainer').appendChild(taskDiv);
             this.modifyTaskListener(project, task, taskDiv);
@@ -381,7 +381,7 @@ class UI {
                         </div>`
 
 
-            taskContainer.innerHTML = '<div id="addTask">+ Add task</div>';
+            taskContainer.innerHTML = '<div id="addTask"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg> Add task</div>';
             taskContainer.appendChild(taskPopUp);
         }
     }
